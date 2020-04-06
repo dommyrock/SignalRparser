d7b8a1afb91c4f35228e3a0774c546c4c5a300ff
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-04-06 19:24:03 +0200

    Updates

---
+++ precache-manifest.24bcec732c4d06c0032ad42722cfc23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5d61bdfb0f6f815d62ef394466d8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f85395d2381a6575b5",
+    "url": "/static/css/main.c9fe23d2.chunk.css"
+  },
+  {
+    "revision": "4583c6ebb34a11722d64",
+    "url": "/static/js/2.b6e0c187.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.b6e0c187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f85395d2381a6575b5",
+    "url": "/static/js/main.4876fc64.chunk.js"
+  },
+  {
+    "revision": "641be7f03cb438846670",
+    "url": "/static/js/runtime-main.cab1cfa1.js"
+  }
+]);
