8579109c0761d222a02cdae5d4112ab5ac1add14
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-04-12 17:00:15 +0200

    webapp-added toc and fixed HoverableTableExample bug
    bug was that i was setting data constants inside functional component instead of moving them outside of them , so whenever i changed section i was losing data rows

--- StreamOutputWebApp/client-app/src/components/Layout/AppRouter.jsx
+++ StreamOutputWebApp/client-app/src/components/Layout/AppRouter.jsx
@@ -6,8 +6,8 @@ import ExampleHighlighter from "../SyntaxHighlighter/ExampleHighlighter";
 import GithubFeaturedList from "../SyntaxHighlighter/GithubFeatured/GithubFeaturedList";
 import HoverableTableExample from "../SyntaxHighlighter/tables/HoverableTableExample";
 import DraggableExample from "../draggableComponent/DraggableExample";
-//Router info https://reacttraining.com/react-router/web/api/BrowserRouter (browser router /hash router (for static files) should wrap whole App.js in index.js to work properly!)
-const AppRouter = () => {
+
+export default function AppRouter() {
   return (
     <Switch>
       <Route exact path="/" component={ExampleHighlighter} />
@@ -18,5 +18,58 @@ const AppRouter = () => {
       <Route exact path="/drag" component={DraggableExample} />
     </Switch>
   );
-};
-export default AppRouter;
+}
+
+//Scroll to element in view https://stackoverflow.com/questions/43441856/reactjs-how-to-scroll-to-an-element
+// or https://stackoverflow.com/questions/40280369/use-anchors-with-react-router
+
+//Router info https://reacttraining.com/react-router/web/api/BrowserRouter (browser router /hash router (for static files) should wrap whole App.js in index.js to work properly!)
+//could also use next.js routing https://nextjs.org/docs/routing/introduction
+
+//useEffect() ...runs after state update and screen repaint by browser
+//data fetching :https://www.robinwieruch.de/react-hooks-fetch-data
+/*React remembers the effect function you provided, and runs it after flushing changes to the DOM and letting the browser paint the screen.
+ Inside the scope of a single render, props and state stay the same. 
+ 
+ FETCHING  EXAMPLE with query saved to state;
+function Parent() {
+  const [query, setQuery] = useState('react');
+
+  // ✅ Preserves identity until query changes
+  const fetchData = useCallback(() => {
+    const url = 'https://hn.algolia.com/api/v1/search?query=' + query;
+    // ... Fetch data and return it ...
+  }, [query]);  // ✅ Callback deps are OK
+
+  return <Child fetchData={fetchData} />
+}
+
+function Child({ fetchData }) {
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData().then(setData);
+  }, [fetchData]); // ✅ Effect deps are OK
+
+  // ...
+}
+Since fetchData only changes inside Parent when its query state changes, our Child won’t refetch the data until it’s actually necessary for the app.
+*/
+
+//useRef() faq --https://reactjs.org/docs/hooks-faq.html#is-there-something-like-instance-variables
+// Like obj instance variable (mutable)
+/*Example
+ function Example() {
+  const [count, setCount] = useState(0);
+  const latestCount = useRef(count);
+
+  useEffect(() => {
+    // Set the mutable latest value
+    latestCount.current = count;
+    setTimeout(() => {
+      // Read the mutable latest value
+      console.log(`You clicked ${latestCount.current} times`);
+    }, 3000);
+  });
+  // ...
+  */
